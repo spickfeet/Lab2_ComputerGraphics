703375ceec9b2013086e5d2a68fcfec2f6314ec1
spickfeet <slavaspickfeet@mail.ru>
2023-11-20 18:18:55 +0300

    Комменты

--- Lab2_KG/Lab2_KG.cpp
+++ Lab2_KG/Lab2_KG.cpp
@@ -2,35 +2,38 @@
 #include <glut.h>
 
 static GLfloat spin = 0.0;
-bool spinStarter;
-bool isMousePressed;
-int mouseX = 5, mouseY = 5;
-double size = 50;
+bool spinStarter; // Флаг для вращения
+bool isMousePressed; // флаг нажатой мыши
+int mouseX = 5, mouseY = 5; // координаты мыши
+double size = 40; // Размер желтого квадрата
 
-void spinDisplay(void)
+void spinDisplay(void) // Метод вращения 
 {
 	if (spinStarter == false) return;
-	spin = spin + 0.02;
+	spin = spin + 0.1;
 	if (spin > 360.0)
 		spin = spin - 360.0;
 	glutPostRedisplay();
 }
-void mouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON) spinStarter = true;
-	if (button == GLUT_RIGHT_BUTTON) spinStarter = false;
+void mouse(int button, int state, int x, int y) 
+{ 
+	if (button == GLUT_LEFT_BUTTON) spinStarter = true; // запустить вращение
+	if (button == GLUT_RIGHT_BUTTON) spinStarter = false; // остановить вращение
 }
-void mouseMotion(int x, int y) {
+void mouseMotion(int x, int y) // получаем координаты курсора и приводим всё в одну систему координат
+{
 	if (mouseX - size / 2 < x - 400 && mouseX + size / 2 > x - 400 && mouseY - size / 2 < 400 - y && mouseY + size / 2 > 400 - y)
 	{
 		mouseX = x - 400;
 		mouseY = 400 - y;
+		glutPostRedisplay();
 	}
 }
 void keyboard(unsigned char key, int x, int y) {
 	if (key == 'x')
-		spinStarter = true;
+		spinStarter = true; // запустить вращение
 	if (key == 'X')
-		spinStarter = false;
+		spinStarter = false; // остановить вращение
 	if (key == 27)
 		exit(0);
 }
@@ -47,7 +50,8 @@ void display(void)
 	glVertex2f(-15.0, 15.0);
 	glEnd();
 	glPopMatrix();
-
+	
+	// Отрисовать желтый квадрат относительно координат мыши
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 1.0, 0);
 	glVertex2f(mouseX - size / 2, mouseY + size / 2);
@@ -79,7 +83,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow(argv[0]);
+	glutCreateWindow("Квадраты");
 	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
@@ -88,5 +92,5 @@ int main(int argc, char** argv)
 	glutMotionFunc(mouseMotion);
 	glutIdleFunc(spinDisplay);
 	glutMainLoop();
-	return 0;   /* ANSI C requires main to return int. */
+	return 0;
 }
